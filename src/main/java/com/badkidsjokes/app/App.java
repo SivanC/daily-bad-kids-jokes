2006ddbfe286d5c7a2cd6b35c617fb3e31830437
Sivan Cooperman <scoop2001@gmail.com>
2021-01-07 14:37:31 -0800

    Where am i

--- src/main/java/com/badkidsjokes/app/App.java
+++ src/main/java/com/badkidsjokes/app/App.java
@@ -4,31 +4,64 @@ import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
 import javax.activation.*;
+
 import java.time.*;
 
 /**
  * Runs the joke sending app
  *
- * @version 0.0.1
+ * @version 0.1.4
  * @author Sivan Cooperman
  */
 public class App {
     public static void main(String[] args) throws MessagingException, InterruptedException {
+        int hour;
+        int minute;
+
+        // Try to get time from jar arguments
+        try {
+            hour = Integer.parseInt(args[0]);
+            minute = Integer.parseInt(args[1]);
+        } catch ( Exception e ) {
+            hour = 13;
+            minute = 00;
+        }
+
         while ( true ) {
-            LocalTime startup = LocalTime.now();
-            LocalTime jokeTime = LocalTime.of(17, 30);
-            Duration wait = Duration.between(startup, jokeTime);
-            if (wait.isNegative()) {
-                wait = Duration.ofHours(24).minus(Duration.between(jokeTime, startup));
-            }
+            Duration wait = getWait(hour, minute);
+
             System.out.println("Waiting for " + wait.toString() + "...");
             Thread.sleep(wait.toMillis());
+
             JokeGetter jokeGetter = new JokeGetter();
             JokeSender jokeSender = new JokeSender("src/main/resources/data.txt");
 
             String joke = jokeGetter.getJoke();
-            jokeSender.sendMessage(joke);
+            jokeSender.sendText(joke);
+            // Wait two minutes to avoid any shenanigans
             Thread.sleep(120000);
         }
+    }
+
+    /**
+     * Gets a Duration between the current time and the specified hour and
+     * minute, in 24-hour time.
+     *
+     * @param hour The hour in 24-hour time of the end of the duration.
+     * @param minute The minute of the end of the duration.
+     * @return A Duration the length of the time between the current time and
+     * the specified time.
+     */
+    private static Duration getWait(int hour, int minute) {
+        LocalTime startup = LocalTime.now();
+        LocalTime jokeTime = LocalTime.of(hour, minute);
+        Duration wait = Duration.between(startup, jokeTime);
+        // Flip the time around if the duration is negative (e.g. it's 2pm and
+        // the end time is 13:00).
+        if (wait.isNegative()) {
+            wait = Duration.ofHours(24).minus(Duration.between(jokeTime, startup));
+        }
+
+        return wait;
     }
 }
--- src/main/java/com/badkidsjokes/app/JokeSender.java
+++ src/main/java/com/badkidsjokes/app/JokeSender.java
@@ -3,29 +3,40 @@ package com.badkidsjokes.app;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+
 import javax.mail.*;
 import javax.mail.internet.*;
 import javax.activation.*;
 
+import sendinblue.ApiClient;
+import sendinblue.ApiException;
+import sendinblue.Configuration;
+import sendinblue.auth.*;
+
+import sibApi.TransactionalSmsApi;
+
+import sibModel.SendTransacSms;
+import sibModel.SendSms;
+
 /**
- * Sends emails via a Sendinblue SMTP mail server to a given recipient
+ * Sends emails via a Sendinblue SMTP mail server to a given recipient.
+ * Also supports SMS.
  *
- * @version 0.1.0
+ * @version 0.2.0
  * @author Sivan Cooperman
  */
 public class JokeSender {
 
 
-    private String recipients;
+    private String emails;
+    private String texts;
 
     private String sender = "dailybadkidsjokes@gmail.com";
     private String username = "";
     private String password = "";
 
     private String host;
-
-    private Properties properties;
-    private Authenticator authenticator;
+    private String key;
 
     public JokeSender(String filename) {
         // Reading in confidential info
@@ -33,45 +44,90 @@ public class JokeSender {
             File options = new File(filename);
             Scanner scn = new Scanner(options);
 
-            this.username = scn.nextLine().split(":")[1];
-            this.password = scn.nextLine().split(":")[1];
-            this.host = scn.nextLine().split(":")[1];
-            // InternetAddress.parse() takes a comma-separated list of addresses
-            this.recipients = scn.nextLine().split(":")[1];
+            while ( scn.hasNextLine() ) {
+                String[] line = scn.nextLine().split(":");
+                switch( line[0] ) {
+                    case "Username":
+                        this.username = line[1];
+                        break;
+                    case "Password":
+                        this.password = line[1];
+                        break;
+                    case "Host":
+                        this.host = line[1];
+                        break;
+                    case "EmailRecipients":
+                        this.emails = line[1];
+                        break;
+                    case "TextRecipients":
+                        this.texts = line[1];
+                        break;
+                    case "ApiKey":
+                        this.key = line[1];
+                        break;
+                }
+            }
             scn.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
         }
+    }
 
+    public void sendEmail(String body) throws MessagingException {
         // Setting options
-        this.properties = new Properties();
-        this.properties.put("mail.smtp.auth", "true");
-        this.properties.put("mail.smtp.starttls.enable", "true");
-        this.properties.put("mail.smtp.host", host);
-        this.properties.put("mail.smtp.port", "587");
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "587");
 
         // Instantiating authenticator
-        this.authenticator = new Authenticator() {
+        Authenticator authenticator = new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(username, password);
             }
         };
-    }
 
-    public void sendMessage(String body) throws MessagingException {
         Session session = Session.getInstance(properties, authenticator);
 
         try {
             Message message = new MimeMessage(session);
 
             message.setFrom(new InternetAddress(this.sender));
-            message.addRecipients(Message.RecipientType.BCC, InternetAddress.parse(this.recipients));
+            message.addRecipients(Message.RecipientType.BCC, InternetAddress.parse(this.emails));
 
             message.setText(body);
 
             Transport.send(message);
         } catch(MessagingException e) {
             System.out.println(e);
+        }
+    }
+
+    public void sendText(String body) {
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+
+        ApiKeyAuth apiKey = (ApiKeyAuth) defaultClient.getAuthentication("api-key");
+        apiKey.setApiKey(this.key);
+
+        ApiKeyAuth partnerKey = (ApiKeyAuth) defaultClient.getAuthentication("partner-key");
+        partnerKey.setApiKey(this.key);
+
+        TransactionalSmsApi apiInstance = new TransactionalSmsApi();
+
+        for ( String recipient : this.texts.split(",") ) {
+            SendTransacSms sendTransacSms = new SendTransacSms(); // SendTransacSms | Values to send a transactional SMS
+            sendTransacSms.setSender("Bad Jokes");
+            sendTransacSms.setRecipient(recipient);
+            sendTransacSms.setContent(body);
+
+            try {
+                SendSms result = apiInstance.sendTransacSms(sendTransacSms);
+                System.out.println(result);
+            } catch (ApiException e) {
+                System.err.println("Exception when calling TransactionalSmsApi#sendTransacSms");
+                e.printStackTrace();
+            }
         }
     }
 }
